420099793ea5f37e828f6b24a0858336566f45c2
whenhowlee <whenhowlee@qq.com>
2015-07-12 07:54:07 +0800

    Update db.sql

--- shorturl/db.sql
+++ shorturl/db.sql
@@ -1,6 +1,6 @@
 /* MySQL 数据库 */
-/* 创建用户 'py'，host 为 'localhost'，密码为 'py_passwd'，该用户对数据库 shorturl 下所有表拥有所有权限 */
-grant all on shorturl.* to 'py'@'localhost' identified by "py_passwd";
+/* 创建用户 'wh'，host 为 'localhost'，密码为 'wh_passwd'，该用户对数据库 shorturl 下所有表拥有所有权限 */
+grant all on shorturl.* to 'wh'@'localhost' identified by "wh_passwd";
 
 create database shorturl default charset utf8;
 
